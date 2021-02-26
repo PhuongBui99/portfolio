dee14eae88dd1600d8c6dd3e92123bf0b49cf5ca
PhuongBui99 <69580574+PhuongBui99@users.noreply.github.com>
2021-02-26 10:36:25 +0700

    xin_chao

--- main.js
+++ main.js
@@ -1,55 +1,14 @@
-const projects = [
-    {
-        id: 'HTML/CSS',
-        src: 'pic1.PNG'
-    },
-    {
-        id: 'Javascript',
-        src: 'song.PNG'
-    },
-    {
-        id: 'HTML/CSS',
-        src: 'Work.PNG'
-    },
-    {
-        id: 'HTML/CSS',
-        src: 'pic1.PNG'
-    },
-    {
-        id: 'Javascript',
-        src: 'song.PNG'
-    },
-    {
-        id: 'API',
-        src: 'capture.PNG'
-    },
-]
-
-window.addEventListener('DOMContentLoaded', function(){
-    displayProjects(projects);
-    filterProject();
-});
-
-function displayProjects(projects){
-
-     const menu = document.getElementById('menu-picture');
-     const project = projects.map(function(item){
-        return `<div class="item col-md-6 col-sm-6 col-lg-4" style="margin-bottom: 30px"><img src="image/${item.src}"></div>`;
-     });
-    menu.innerHTML = project.join('');
-}
-
-function filterProject(){
-    const btns = document.querySelectorAll('.filter');
-    btns.forEach(function (btn){
-        btn.addEventListener("click", function(e){
-            e.preventDefault();
-            const typeProject = e.currentTarget.id;
-            const filter = projects.filter(function (project){
-                if(project.id === typeProject)return project;
-            });
-            if(typeProject === "All")displayProjects(projects);
-            else displayProjects(filter);
-        })
-    })
-}
+<<<<<<< HEAD
+$(document).ready(function () {
+    $("#single_item").slick({
+      
+    });  
+  });
+=======
+$(document).ready(function () {
+    $("#single_item").slick({
+      
+    });  
+  });
+>>>>>>> ccae73a57ab644846c68d6990fca7ef84ea9363d
+  
